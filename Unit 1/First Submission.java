45e8a253e2a642e2fb1dd1c7c184959202bc92ff
KaylaMori <kaylamorigawara@gmail.com>
2025-08-13 08:33:16 -1000

    Create First Submission.java

---
+++ Unit 1/First Submission.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+      //Task 1: Print "Hello World" just once
+      System.out.print("Hello World ");
+     //Task 2: Print "Hello World" once again next to the previous one and print it again on the next line with ln on the first line after the word "print"
+      System.out.println("Hello World");
+      System.out.print("Hello World");
+  }
+}
