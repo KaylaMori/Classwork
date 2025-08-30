c6106d2481e8b5cbd10338ad50099a90e1639596
KaylaMori <kaylamorigawara@gmail.com>
2025-08-29 20:16:28 -1000

    Create cubeHomework.java

---
+++ Unit 1/cubeHomework.java
@@ -0,0 +1,41 @@
+public class cubeHW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+/**
+* This code calculates the surface area and volume of a cube.
+*/
+
+/*
+Line 17 declares the sideLength as a double and the number of which it is measured at.
+The sideLength is used to calculate the surfaceArea and volume when plugged into
+the codes (equations) to find the measurements of each aspect of the cube.
+*/
+		
+double sideLength = 9;
+
+/* 
+Line 24 declares the surfaceArea as a double and the equation used to find the measurement.
+The equation being 6*(sideLength*sideLength) which is the formula: A=6a^2 in which a = sideLength.
+*/
+
+double surfaceArea = 6*(sideLength*sideLength);
+
+/* 
+Line 31 declares the volume as a double and the equation used to find its measurement. 
+The equation being "sideLength*sideLength*sideLength" which is the formula: V=s^3 in which s = sideLength
+*/
+
+double volume = sideLength * sideLength * sideLength;
+
+/* 
+Line 37 prints the sentence of what the surfaceArea and volume of the cube is from lines 24 & 31.
+*/
+
+System.out.print("The surface area of the cube is " + (int) surfaceArea + " and the volume is " + (int) volume);
+
+		
+	}
+
+}
