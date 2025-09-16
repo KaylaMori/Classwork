30ce29f916864b1c4efe1e328ebcb4737c631a06
KaylaMori <kaylamorigawara@gmail.com>
2025-09-15 14:15:31 -1000

    Create CodingBatOne.java

---
+++ Unit 1/CodingBatOne.java
@@ -0,0 +1,31 @@
+
+public class codingBatOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//1: helloName
+		
+		public String helloName(String name) {
+			  return ("Hello "+name+"!");
+			}
+		
+		//2: makeAbba
+		public String makeAbba(String a, String b) {
+			  return (a+b+b+a);
+			}
+		
+		//3: makeTags
+		public String makeTags(String tag, String word) {
+			  
+			  return ("<" + tag + ">" + word + "</" + tag + ">");
+			  
+			}
+		//4: firstHalf
+		public String firstHalf(String str) {
+			  return (str.substring(0,str.length()/2));
+			}
+		
+	}
+
+}
