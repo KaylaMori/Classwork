a5a6ed1336c3efc10e795eabcc7e5c4820c48a25
KaylaMori <kaylamorigawara@gmail.com>
2025-09-19 09:18:55 -1000

    Update ShapesMain.java

--- Unit 1/ObjectsIntroPractice/ShapesMain.java
+++ Unit 1/ObjectsIntroPractice/ShapesMain.java
@@ -1,25 +1,39 @@
+/*
+ * Shapes main class 
+ */
 public class ShapesMain {
 
 	public static void main(String[] args) 
 	{
 		/*
-		 * call constructor to generate a new Circle object w/ radius of __
+		 * call constructor to generate a new Circle object w/ radius of
 		 */
 		Circle myCirc1 = new Circle (1.9);
 		Circle myCirc2 = new Circle (2.7);
 		
-		
+		/*
+		 * Trys to print out Circles, but given reference code
+		 */
 		System.out.println(myCirc1);
 		System.out.println(myCirc2);
 
 		Circle myCirc3 = myCirc1;
 		System.out.println(myCirc3);
 		
-		
+		/*
+		 * Prints out original radius of Circle 1
+		 */
 		System.out.println(myCirc1.getRadius());
-	
+		
+		/*
+		 * Prints out new Radius that circle 1 is set to 
+		 */
 		myCirc1.setRadius(7.0);
 		System.out.println(myCirc1.getRadius());
+		
+		/*
+		 * Prints out circumference, area, degrees, and what shape is 
+		 */
 		System.out.println(myCirc1.getCircumf());
 		System.out.println(myCirc1.getArea());
 		System.out.println(Circle.getDegrees());
