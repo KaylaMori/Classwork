a1958748b2801d3179c1743f9d9d3e5b53f1c3eb
KaylaMori <kaylamorigawara@gmail.com>
2025-10-03 10:26:26 -1000

    Create ProjectMain.java

---
+++ Unit 1/ShapesProject/ProjectMain.java
@@ -0,0 +1,328 @@
+/*
+ * Shapes main class
+ */
+public class projectMain {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Call constructor to generate a new Circle object with a radius of 1.9
+		 */
+		Circle myCircle = new Circle (1.9);
+		/*
+		 * Prints out the Circumference, Area, and Degrees  of myCircle
+		 */
+		System.out.println(myCircle.getCircumf());
+		System.out.println(myCircle.getArea());
+		System.out.println(Circle.getDegrees());
+		/*
+		 * Prints out the original radius of myCircle
+		 */
+		System.out.println(myCircle.getRadius());
+		/*
+		 * Prints out new radius that myCircle is set to
+		 */
+		myCircle.setRadius(7.0);
+		System.out.println(myCircle.getRadius());
+		
+		
+		/*
+		 * Call constructor to generate a new Circle object with a radius of 1.4
+		 */
+		Circle myCircle2 = new Circle (1.4);
+		/*
+		 * Prints out the Circumference, Area, and Degrees of myCircle2
+		 */
+		System.out.println(myCircle2.getCircumf());
+		System.out.println(myCircle2.getArea());
+		System.out.println(Circle.getDegrees());
+		/*
+		 * Prints out the original radius of myCircle2
+		 */
+		System.out.println(myCircle2.getRadius());
+		/*
+		 * Prints out new radius that myCircle2 is set to
+		 */
+		myCircle2.setRadius(7.0);
+		System.out.println(myCircle2.getRadius());
+		
+		
+		/*
+		 * Call constructor to generate a new Circle object with a radius of 6.5
+		 */
+		Circle myCircle3 = new Circle (6.5);
+		/*
+		 * Prints out the Circumference, Area, and Degrees of myCircle3
+		 */
+		System.out.println(myCircle3.getCircumf());
+		System.out.println(myCircle3.getArea());
+		System.out.println(Circle.getDegrees());
+		/*
+		 * Prints out the original radius of myCircle3
+		 */
+		System.out.println(myCircle3.getRadius());
+		/*
+		 * Prints out new radius that myCircle3 is set to
+		 */
+		myCircle3.setRadius(7.0);
+		System.out.println(myCircle3.getRadius());
+		
+		
+		
+		
+		/*
+		 * Call constructor to generate a new Square object with a length of 4.2
+		 */
+		Square mySquare = new Square (4.2);
+		/*
+		 * Prints out the Perimeter, Area, and Name of mySquare
+		 */
+		System.out.println(mySquare.getPerimeter());
+		System.out.println(mySquare.getArea());
+		System.out.println(Square.getName());
+		/*
+		 * Prints out the original length of mySquare
+		 */
+		System.out.println(mySquare.getLength());
+		/*
+		 * Prints out new length that mySquare is set to
+		 */
+		mySquare.setLength(7.0);
+		System.out.println(mySquare.getLength());
+		
+		
+		/*
+		 * Call constructor to generate a new Square object with a length of 5.1
+		 */
+		Square mySquare2 = new Square (5.1);
+		/*
+		 * Prints out the Perimeter, Area, and Name of mySquare2
+		 */
+		System.out.println(mySquare2.getPerimeter());
+		System.out.println(mySquare2.getArea());
+		System.out.println(Square.getName()); 
+		/*
+		 * Prints out the original length of mySquare2
+		 */
+		System.out.println(mySquare2.getLength());
+		/*
+		 * Prints out new length that mySquare2 is set to
+		 */
+		mySquare2.setLength(7.0);
+		System.out.println(mySquare2.getLength());
+		
+		
+		/*
+		 * Call constructor to generate a new Square object with a length of 9.2
+		 */
+		Square mySquare3 = new Square (9.2);
+		/*
+		 * Prints out the Perimeter, Area, and Name of mySquare3
+		 */
+		System.out.println(mySquare3.getPerimeter());
+		System.out.println(mySquare3.getArea());
+		System.out.println(Square.getName());
+		/*
+		 * Prints out the original length of mySquare3
+		 */
+		System.out.println(mySquare3.getLength());
+		/*
+		 * Prints out new length that mySquare3 is set to
+		 */
+		mySquare3.setLength(7.0);
+		System.out.println(mySquare3.getLength());
+		
+		
+		
+		
+		/*
+		 * Call constructor to generate a new Cube object with a length of 2.6
+		 */
+        Cube myCube = new Cube (2.6);
+		/*
+		 * Prints out the Total Surface Area, Volume, and Name of myCube
+		 */
+		System.out.println(myCube.getTotalSurfaceArea());
+		System.out.println(myCube.getVolume());
+		System.out.println(Cube.getName());
+		/*
+		 * Prints out the original length of myCube
+		 */
+		System.out.println(myCube.getLength());
+		/*
+		 * Prints out new length that myCube is set to
+		 */
+		myCube.setLength(7.0);
+		System.out.println(myCube.getLength());
+		
+		
+		/*
+		 * Call constructor to generate a new Cube object with a length of 7.8
+		 */
+		Cube myCube2 = new Cube (7.8);
+		/*
+		 * Prints out the Total Surface Area, Volume, and Name of myCube
+		 */
+		System.out.println(myCube2.getTotalSurfaceArea());
+		System.out.println(myCube2.getVolume());
+		System.out.println(Cube.getName());
+		/*
+		 * Prints out the original length of myCube2
+		 */
+		System.out.println(myCube2.getLength());
+		/*
+		 * Prints out new length that myCube2 is set to
+		 */
+		myCube2.setLength(7.0);
+		System.out.println(myCube2.getLength());
+		
+		
+		/*
+		 * Call constructor to generate a new Cube object with a length of 0.3
+		 */
+		Cube myCube3 = new Cube (0.3);
+		/*
+		 * Prints out the Total Surface Area, Volume, and Name of myCube
+		 */
+		System.out.println(myCube3.getTotalSurfaceArea());
+		System.out.println(myCube3.getVolume());
+		System.out.println(Cube.getName());
+		/*
+		 * Prints out the original length of myCube3
+		 */
+		System.out.println(myCube3.getLength());
+		/*
+		 * Prints out new length that myCube3 is set to
+		 */
+		myCube3.setLength(7.0);
+		System.out.println(myCube3.getLength());
+		
+		
+		
+		
+		/*
+		 * Call constructor to generate a new Rectangle object with a length of 3.7
+		 */
+		Rectangle myRectangle = new Rectangle (3.7);
+		/*
+		 * Prints out the Area, Perimeter, and Name of myRectangle
+		 */
+		System.out.println(myRectangle.getArea());
+		System.out.println(myRectangle.getPerimeter());
+		System.out.println(Rectangle.getName());
+		/*
+		 * Prints out the original length of myRectangle
+		 */
+		System.out.println(myRectangle.getLength());
+		/*
+		 * Prints out new length that myRectangle is set to
+		 */
+		myRectangle.setLength(7.0);
+		System.out.println(myRectangle.getLength());
+		
+		
+		/*
+		 * Call constructor to generate a new Rectangle object with a length of 5.9
+		 */
+		Rectangle myRectangle2 = new Rectangle (5.9);
+		/*
+		 * Prints out the Area, Perimeter, and Name of myRectangle
+		 */
+		System.out.println(myRectangle2.getArea());
+		System.out.println(myRectangle2.getPerimeter());
+		System.out.println(Rectangle.getName());
+		/*
+		 * Prints out the original length of myRectangle2
+		 */
+		System.out.println(myRectangle2.getLength());
+		/*
+		 * Prints out new length that myRectangle2 is set to
+		 */
+		myRectangle2.setLength(7.0);
+		System.out.println(myRectangle2.getLength());
+		
+		
+		/*
+		 * Call constructor to generate a new Rectangle object with a length of 4.3
+		 */
+		Rectangle myRectangle3 = new Rectangle (4.3);
+		/*
+		 * Prints out the Area, Perimeter, and Name of myRectangle
+		 */
+		System.out.println(myRectangle3.getArea());
+		System.out.println(myRectangle3.getPerimeter());
+		System.out.println(Rectangle.getName());
+		/*
+		 * Prints out the original length of myRectangle3
+		 */
+		System.out.println(myRectangle3.getLength());
+		/*
+		 * Prints out new length that myRectangle3 is set to
+		 */
+		myRectangle3.setLength(7.0);
+		System.out.println(myRectangle3.getLength());
+		
+		
+		
+		
+		/*
+		 * Call constructor to generate a new Sphere object with a radius of 6.5
+		 */
+		Sphere mySphere = new Sphere (6.5);
+		/*
+		 * Prints out the Surface Area, Diameter, and Degrees of mySphere
+		 */
+		System.out.println(mySphere.getSurfaceArea());
+		System.out.println(mySphere.getDiameter());
+		System.out.println(Sphere.getDegrees());
+		/*
+		 * Prints out the original radius of mySphere
+		 */
+		System.out.println(mySphere.getRadius());
+		/*
+		 * Prints out new radius that mySphere is set to
+		 */
+		mySphere.setRadius(7.0);
+		System.out.println(mySphere.getRadius());
+		
+		
+		/*
+		 * Call constructor to generate a new Sphere object with a radius of 7.2
+		 */
+		Sphere mySphere2 = new Sphere (7.2);
+		
+		System.out.println(mySphere2.getSurfaceArea());
+		System.out.println(mySphere2.getDiameter());
+		System.out.println(Sphere.getDegrees());
+		/*
+		 * Prints out the original radius of mySphere2
+		 */
+		System.out.println(mySphere2.getRadius());
+		/*
+		 * Prints out new radius that mySphere2 is set to
+		 */
+		mySphere2.setRadius(7.0);
+		System.out.println(mySphere2.getRadius());
+		
+		
+		/*
+		 * Call constructor to generate a new Sphere object with a radius of 5.8
+		 */
+		Sphere mySphere3 = new Sphere (5.8);
+		
+		System.out.println(mySphere3.getSurfaceArea());
+		System.out.println(mySphere3.getDiameter());
+		System.out.println(Sphere.getDegrees());
+		/*
+		 * Prints out the original radius of mySphere3
+		 */
+		System.out.println(mySphere3.getRadius());
+		/*
+		 * Prints out new radius that mySphere3 is set to
+		 */
+		mySphere3.setRadius(7.0);
+		System.out.println(mySphere3.getRadius());
+		
+	}
+
+}
