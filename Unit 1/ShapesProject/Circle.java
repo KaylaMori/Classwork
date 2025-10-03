847ab38e725b21a5224144e5b2fd33899ec01f01
KaylaMori <kaylamorigawara@gmail.com>
2025-10-03 10:27:22 -1000

    Create Circle.java

---
+++ Unit 1/ShapesProject/Circle.java
@@ -0,0 +1,61 @@
+/*
+ * Circle class without main method
+ */
+public class Circle 
+{
+	/*
+	 * Declares radius as double and pi as final double which is 3.14
+	 */
+	private double radius; 
+	private final double pi = 3.14;
+
+	/*
+	 * When called, generates new Circle object with the specified radius, r 
+	 */
+	public Circle (double r)
+	{
+		radius = r;
+	}
+	
+	/*
+	 * Returns the radius 
+	 */
+	public double getRadius()
+	{
+		return radius;
+	}
+	
+	/*
+	 * Changes circle objects radius as a new Radius 
+	 */
+	public void setRadius(double newRadius)
+	{
+		radius = newRadius;
+	}
+	
+	/*
+	 * returns the equation to get the Circumference
+	 */
+	public double getCircumf()
+	{
+		return (2*pi*radius);
+	}
+	
+	/*
+	 * returns the equation to get the Area
+	 */
+	public double getArea()
+	{
+		return (pi*(radius*radius));
+	}
+	
+	/*
+	 * returns the degrees of a circle
+	 */
+	public static int getDegrees()
+	{
+		return 360;
+	}
+	
+}
+		
