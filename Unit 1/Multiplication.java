f279cbd29f130fe071b7774ee1011bd47b41e8d3
KaylaMori <kaylamorigawara@gmail.com>
2025-11-07 10:04:38 -1000

    Create Multiplication.java

---
+++ Unit 1/Multiplication.java
@@ -0,0 +1,20 @@
+
+public class testing {
+
+
+
+	public int multiply(int a, int b)
+	{
+		int count =0;
+		int sum = 0;
+		while(count<a)
+		{
+			
+			sum+=b;
+			count++;
+		}
+		return sum;
+	}
+	
+	
+}
