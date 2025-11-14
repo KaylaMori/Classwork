a607b7eb08d448a51187a713b00ca9ee28e1f0fa
KaylaMori <kaylamorigawara@gmail.com>
2025-11-14 09:23:01 -1000

    Create StringCodingbat3.java

---
+++ Unit 1/StringCodingbat3.java
@@ -0,0 +1,14 @@
+public class StringCodingbat3 {
+
+	
+	//doubleChar()
+	public String doubleChar(String str) {
+		  String result = "";
+		  for(int i=0; i<str.length();i++)
+		  {
+		    result = result + str.charAt(i)+str.charAt(i);
+		  }
+		  return result;
+		}
+
+}
