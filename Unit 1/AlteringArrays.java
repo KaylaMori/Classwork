a3744dbbf16fc3ffbb906570a4fae25cbf2df3a2
KaylaMori <kaylamorigawara@gmail.com>
2026-01-21 13:45:35 -1000

    Update AlteringArrays.java

--- Unit 1/AlteringArrays.java
+++ Unit 1/AlteringArrays.java
@@ -1,8 +1,12 @@
-	public static void subtract5FromAll4(int[]nums)
+//method that subtracts 5 from each number in the array
+public static void subtract5FromAll4(int[]nums)
 	{
+		//creates a copy of the array by creating a new one the same length
 		int [] copyArray = new int[nums.length];
+		//a for loop that goes through each number in the array
 		for(int i=0;i<nums.length; i++)
 		{
+			//the array that is copied subtracts 5 from each number
 			copyArray[i] = nums[i]-5;
 		}
 	}
